503f4bf797386fb84befdbc4db09b0544e0143bf
nanu <ranusate19@gmail.com>
2025-10-12 18:54:19 +0800

    update

--- resources/views/livewire/laporan-surat.blade.php
+++ resources/views/livewire/laporan-surat.blade.php
@@ -19,7 +19,6 @@
                             <input type="date" id="tanggal_mulai" wire:model="tanggal_mulai" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm">
                             @error('tanggal_mulai') <span class="text-xs text-red-600">{{ $message }}</span> @enderror
                         </div>
-
                         <div>
                             <label for="tanggal_selesai" class="block text-sm font-medium text-gray-700">Sampai Tanggal</label>
                             <input type="date" id="tanggal_selesai" wire:model="tanggal_selesai" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm">
@@ -27,42 +26,26 @@
                         </div>
                     </div>
 
-                    <div class="mt-8 text-right space-x-3">
-                        <button type="submit" wire:loading.attr="disabled" wire:target="generatePdf" class="inline-flex items-center justify-center px-6 py-3 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50">
-                            <span  wire:target="generatePdf" class="inline-flex items-center">
+                    <div class="mt-8 flex flex-col md:flex-row justify-end space-y-2 md:space-y-0 md:space-x-3 me-2 mb-2">
+                        <button type="submit" wire:loading.attr="disabled" wire:target="generatePdf" class="inline-flex items-center justify-center px-6 py-3 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50 w-full md:w-auto">
+                            <span wire:target="generatePdf" class="inline-flex items-center">
                                 <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path>
                                 </svg>
                                 Unduh Laporan PDF
                             </span>
-
-                            {{-- <span wire:target="generatePdf" class="inline-flex items-center">
-                                <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                </svg>
-                                Memproses...
-                            </span> --}}
                         </button>
 
-                        <button type="button" wire:click="generateExcel" wire:loading.attr="disabled" wire:target="generateExcel" class="inline-flex items-center justify-center px-6 py-3 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 disabled:opacity-50">
-
-                            <span  wire:target="generateExcel" class="inline-flex items-center">
+                        <button type="button" wire:click="generateExcel" wire:loading.attr="disabled" wire:target="generateExcel" class="inline-flex items-center justify-center px-6 py-3 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 disabled:opacity-50 w-full md:w-auto">
+                            <span wire:target="generateExcel" class="inline-flex items-center">
                                 <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path>
                                 </svg>
                                 Unduh Excel
                             </span>
-
-                            {{-- <span  wire:target="generateExcel" class="inline-flex items-center">
-                                <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                </svg>
-                                Memproses...
-                            </span> --}}
                         </button>
                     </div>
+
                 </form>
             </div>
         </div>
